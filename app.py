8b62c5871a42698bebd03a2607c12f5333f2a5a0
testrun7788 <testrun7788@gmail.com>
2025-12-24 08:02:33 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from calculator import add, subtract
+from utils import greet
+
+def main():
+    greet("Salman")
+
+    print("Add: ", add(10, 5))
+    print("Subtract: ", subtract(10, 5))
+
+if __name__ == "__main__":
+    main()
